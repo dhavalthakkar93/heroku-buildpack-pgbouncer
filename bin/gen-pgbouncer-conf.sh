9440f4faf45cf6b47b54b0d894be11afe946fc6e
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2019-07-19 19:59:31 +0530

    Update gen-pgbouncer-conf.sh

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -13,7 +13,7 @@ fi
 
 cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
 [pgbouncer]
-listen_addr = ${HEROKU_PRIVATE_IP}
+listen_addr = pgbouncer-internal-1.herokuapp.com
 listen_port = 6000
 auth_type = md5
 auth_file = /app/vendor/pgbouncer/users.txt
